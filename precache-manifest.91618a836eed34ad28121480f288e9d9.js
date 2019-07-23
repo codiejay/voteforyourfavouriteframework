aff02f517c24ac11f2ba8c328e0bc46f0e19fc7b
James Akpan <arfitcode@gmail.com>
2019-07-23 21:00:11 +0100

    Updates

---
+++ precache-manifest.91618a836eed34ad28121480f288e9d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353113e2ee945406aced4b997c994d8d",
+    "url": "/voteforyourfavouriteframework/index.html"
+  },
+  {
+    "revision": "cdd66224a00d2a3cb052",
+    "url": "/voteforyourfavouriteframework/static/css/main.da6a2b41.chunk.css"
+  },
+  {
+    "revision": "6b235f89fd39eed58fc8",
+    "url": "/voteforyourfavouriteframework/static/js/2.a928435b.chunk.js"
+  },
+  {
+    "revision": "cdd66224a00d2a3cb052",
+    "url": "/voteforyourfavouriteframework/static/js/main.4b61dcfd.chunk.js"
+  },
+  {
+    "revision": "628786895542a78d2dc3",
+    "url": "/voteforyourfavouriteframework/static/js/runtime~main.bb5eb468.js"
+  },
+  {
+    "revision": "4ee99cd26074cbbc5d11b4c41c4cd57d",
+    "url": "/voteforyourfavouriteframework/static/media/acme.4ee99cd2.wav"
+  },
+  {
+    "revision": "c10c6d6d8ee3fd097f627869a1757658",
+    "url": "/voteforyourfavouriteframework/static/media/react.c10c6d6d.svg"
+  },
+  {
+    "revision": "914933feaef3d4a1926c434d6c43be1b",
+    "url": "/voteforyourfavouriteframework/static/media/spinner.914933fe.gif"
+  },
+  {
+    "revision": "c27f74e48e2b0b63cbbb52088c6cc1e1",
+    "url": "/voteforyourfavouriteframework/static/media/vue.c27f74e4.svg"
+  }
+]);
